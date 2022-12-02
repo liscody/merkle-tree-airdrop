d6a4ad1f1e891d08571190e859c7822bc70dcc09
Ihor Antsupov <i.antsupov@gmail.com>
2022-12-02 07:55:52 +0200

    fin

--- contracts/MerkleAirDrop.sol
+++ contracts/MerkleAirDrop.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-
-contract MerkleClaimERC20 is Ownable {
-        using ECDSA for bytes32;
+contract MerkleAirDrop is Ownable {
+    using ECDSA for bytes32;
 
     IERC20 public token;
     bytes32 public merkleRoot;
@@ -22,36 +21,35 @@ contract MerkleClaimERC20 is Ownable {
 
     constructor(bytes32 _merkleRoot, uint256 _amount) {
         merkleRoot = _merkleRoot;
-        amount = _amount; 
+        amount = _amount;
     }
 
     event Claim(address indexed to, uint256 amount);
 
-    function claim(
-        bytes32[] calldata proof
-    ) public {
-        // Throw if address has already claimed tokens
-        if (merkleRoot == 0) revert IsActive();
+    function claim(bytes32[] calldata proof) public {
+        // if (merkleRoot == 0) revert IsActive();
         if (claimed[msg.sender]) revert AlreadyClaimed();
 
-        // Verify merkle proof, or revert if not in tree
         bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
 
         bool isValidLeaf = MerkleProof.verify(proof, merkleRoot, leaf);
         if (!isValidLeaf) revert NotInMerkle();
 
-        // Set address to claimed
         claimed[msg.sender] = true;
 
         IERC20(token).transfer(msg.sender, amount);
 
-        // Emit claim event
         emit Claim(msg.sender, amount);
     }
 
     function setRoot(bytes32 newRoot) public onlyOwner {
         merkleRoot = newRoot;
     }
+
+    function setToken(address newToken) public onlyOwner {
+        token = IERC20(newToken);
+    }
+
     function setAmount(uint256 _newAmount) public onlyOwner {
         amount = _newAmount;
     }
---
+++ test/MerkleAirDrop.test.ts
@@ -0,0 +1,106 @@
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { MerkleTree } from "merkletreejs"; // MerkleTree.js
+import { takeSnapshot } from "@nomicfoundation/hardhat-network-helpers";
+import type { MerkleAirDrop } from "../typechain-types";
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import type { SnapshotRestorer } from "@nomicfoundation/hardhat-network-helpers";
+
+const keccak256 = require("keccak256");
+
+describe("MerkleAirDrop", function () {
+    let snapshotA: SnapshotRestorer;
+    // Signers.
+    let deployer: SignerWithAddress, owner: SignerWithAddress, user: SignerWithAddress, claimer: SignerWithAddress;
+
+    let merkleAirDrop: MerkleAirDrop;
+    let myErc20: any;
+    let hexProof: any;
+    let root: any;
+
+    before(async () => {
+        // Getting of signers.
+        [deployer, user, claimer] = await ethers.getSigners();
+
+        const leaves = [
+            claimer.address,
+            "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",
+            "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",
+            "0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB",
+            "0x617F2E2fD72FD9D5503197092aC168c91465E7f2",
+            "0x17F6AD8Ef982297579C203069C1DbfFE4348c372",
+            "0x5c6B0f7Bf3E7ce046039Bd8FABdfD3f9F5021678",
+            "0x1aE0EA34a72D944a8C7603FfB3eC30a6669E454C"
+        ];
+
+        const leafsNode = leaves.map((addr) => keccak256(addr));
+        // console.log(leafsNode);
+
+        const merkleTree = new MerkleTree(leafsNode, keccak256, { sortPairs: true });
+        root = "0x" + merkleTree.getRoot().toString("hex");
+        console.log(root, "root");
+        console.log(merkleTree.toString());
+
+        const claimerAddress = leafsNode[0];
+        hexProof = merkleTree.getHexProof(claimerAddress);
+
+        console.log(hexProof, "hexProof");
+        console.log(claimerAddress, "claimerAddress");
+
+        // Deployment of the factory.
+        const MyERC20 = await ethers.getContractFactory("MyERC20", deployer);
+        myErc20 = await MyERC20.deploy();
+        await myErc20.deployed();
+
+        const MerkleAirDrop = await ethers.getContractFactory("MerkleAirDrop", deployer);
+        merkleAirDrop = await MerkleAirDrop.deploy(root, 1000000);
+        await merkleAirDrop.deployed();
+
+        await myErc20.transfer(merkleAirDrop.address, 10000000000000);
+
+        owner = deployer;
+        await merkleAirDrop.connect(owner).setToken(myErc20.address);
+
+        snapshotA = await takeSnapshot();
+    });
+
+    afterEach(async () => await snapshotA.restore());
+
+    describe("Testing claim function", function () {
+        it("Should reverted with custom error 'AlreadyClaimed'", async () => {
+            await merkleAirDrop.connect(claimer).claim(hexProof);
+            await expect (merkleAirDrop.connect(claimer).claim(hexProof)).to.be.revertedWithCustomError(
+                merkleAirDrop,
+                "AlreadyClaimed"
+            );
+        });
+        
+        it("Should reverted with custom error 'NotInMerkle'", async () => {
+            await expect (merkleAirDrop.connect(owner).claim(hexProof)).to.be.revertedWithCustomError(
+                merkleAirDrop,
+                "NotInMerkle"
+            );
+        }); 
+        
+        it("Should change balance after claim", async () => {
+            await (merkleAirDrop.connect(claimer).claim(hexProof));
+            let balance = await myErc20.balanceOf(claimer.address);
+                expect(balance).to.be.equal(1000000);
+        });
+    });
+
+    describe("Testing setRoot function", function () {
+        it("Should change a root", async () => {
+            let newRoot = root;
+            await merkleAirDrop.connect(owner).setRoot(newRoot);
+            expect (root).to.be.equal(newRoot)
+        });
+    });
+    describe("Testing setAmount function", function () {
+        it("Should change a amount", async () => {
+            let newAmount = 5000000;
+            await merkleAirDrop.connect(owner).setAmount(newAmount);
+            expect (newAmount).to.be.equal(5000000)
+        });
+    });
+});
